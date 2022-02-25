0f787feb59ca85a5841d5639bded13e11415a6fe
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-24 21:22:33 -0300

    Creating new project and put image and sounds
    Criando mais um projeto e adicionando imagens e musica.

---
+++ MovieQuiz/MovieQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Rennan Bruno on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
